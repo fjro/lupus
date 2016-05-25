e5eecdee977e2f3a095146a69710bed9628b937e
julian <julianroche@gmail.com>
2016-05-25 12:31:50 +0100

    Reformatted and commented.

--- R/eda/corrplots.R
+++ R/eda/corrplots.R
@@ -1,32 +1,33 @@
-#draw corrplots
-#make corrplots
-corrplot(allCM, order = "hclust", method="color")
+#draw corrplots to visualise differences in various measures of gene association 
+#between lupus patients and controls
+library(corrplot)
+corrplot(allCM, order <- "hclust", method="color")
 corrplot(as.matrix(aa), method="color", type='upper')
 par(mfrow=c(3,2))
-colnames(controlCM) = 1:94
+colnames(controlCM) <- 1:94
 corrplot(controlCM, title="", order = "original", method="color", tl.cex = .3, type='upper', tl.pos = 'd')
 
-colnames(lupusCM) = 1:94
+colnames(lupusCM) <- 1:94
 corrplot(lupusCM, title="", order = "original", method="color",  tl.cex = .3, type='upper', tl.pos = 'd')
 #controls vs lupus appear different, clusters of high positive and negative correlations
 
-#A corrplot
-vv = controlA
-colnames(vv) = 1:94
-vv = as.matrix(vv) #correlation adjust
+#'A' based corrplot
+vv <- controlA
+colnames(vv) <- 1:94
+vv <- as.matrix(vv) # sign adjust using Pearson correlation to indicate direction of association
 dP <- vv * sign(controlCM)
 corrplot(dP, title="", order = "original", method="color", tl.cex = .3, type='upper', tl.pos = 'd')
 
-bb = lupusA
+bb <- lupusA
 colnames(bb) = 1:94
-bb = as.matrix(bb)
+bb <- as.matrix(bb)
 dP <- bb * sign(lupusCM)
 corrplot(dP, title="", order = "original", method="color", tl.cex = .3, type='upper', tl.pos = 'd')
 
 #dcor plots
-dvv = tadcor(genes[genes$diseased=='Control',-c(1,96,97)])
+dvv <- tadcor(genes[genes$diseased=='Control',-c(1,96,97)])
 which(dP > 1)
-colnames(dvv) = 1:94
+colnames(dvv) <- 1:94
 #correlation adjust
 dP <- dvv * sign(controlCM)
 corrplot(dP, title="", order = "original", method="color", tl.cex = .3, type='upper', tl.pos = 'd')
@@ -36,5 +37,5 @@ colnames(dbb) = 1:94
 dP <- dbb * sign(lupusCM)
 corrplot(dP, title="", order = "original", method="color", tl.cex = .3, type='upper', tl.pos = 'd')
 
-controlDcor = tadcor(genes[genes$diseased=='Control',-c(1,96,97)])
-lupusDcor = tadcor(genes[genes$diseased=='Lupus',-c(1,96,97)])
+controlDcor <- tadcor(genes[genes$diseased=='Control',-c(1,96,97)])
+lupusDcor <- tadcor(genes[genes$diseased=='Lupus',-c(1,96,97)])
