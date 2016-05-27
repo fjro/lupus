85039efc83f8224a2499ea37b429c8279826e5fd
julian <julianroche@gmail.com>
2016-05-28 00:37:06 +0100

    Sketched snowfall based parallelisation of three way association estimates.

--- R/association/permutations.R
+++ R/association/permutations.R
@@ -4,6 +4,8 @@ library(gtools)
 library(stringi)
 library(energy)
 library(matie)
+library(dplyr)
+library(microbenchmark)
 
 #first evaluate the indices of all possible 3 way associations
 pr <- data.frame(permutations(94, 3, 1:94, repeats.allowed = F))
@@ -29,39 +31,61 @@ write.csv(dd, "output/3way.csv")
 dcor(controls[,20], controls[,3:4])
 dcov.test(controls[,20], controls[,3:4])$estimates[2]
 
-#' Computes all 3 way association using dcor, A and R2 and also computes the residual
+#' Computes all 3 way associations using dcor, A and R2 and also computes the residual
 #' non-linear association.
 #' 
 #' @param model A dataframe containing the indices of the 3 way associations.
 #' @param data The data.
 #' @return A dataframe with the results.
-threeWay <- function(model, data)
-{
-  n <- nrow(model)
-  results <- data.frame(rep(0, n), rep(0, n), rep(0, n), rep(0, n), rep(0, n))
-  colnames(results) = c('dcor', 'A', 'R2', 'nldcor', 'nlA')
-  for(i in 1:n)
-  {
-    X = model[i,1]
-    Y = model[i,2]
-    Z = model[i,3]
-    #linear and nonlinear
-    results[i, 1] = dcov.test(data[,X],data[,c(Y,Z)])$estimates[2]
-    results[i, 2] = ma(data.frame(data[,c(Y,Z)],data[,X]))$A
-    res = lm(data[,X] ~ data[,Y] * data[,Z])
-    results[i, 3] = summary(res)$r.squared
-    
-    #residual association
-    results[i, 4] = dcov.test(res$residuals,data[,c(Y,Z)])$estimates[2]
-    results[i, 5] = ma(data.frame(data[,c(Y,Z)],res$residuals))$A
-    
-    if(i %% 100 == 0)
-    {
-      print(paste(toString(i), " tests complete", sep = ""))
-    }
+threeWayAll <- function(models, data, ncores = 'all') {
+  require(data.table, quietly = T)
+  require(snowfall, quietly = T)
+  require(parallel, quietly = T)
+  cat(format(Sys.time(),usetz = TRUE), ": Calculating ", nrow(models), " associations \n")
+  
+  if (ncores == 'all') {
+    ncores <- detectCores() -1
   }
+  
+  sfInit( parallel=TRUE, cpus=ncores, slaveOutfile="log.txt")
+  sfExport('threeWayAssociation')
+  
+  result <- rbindlist(sfLapply(models,
+                               threeWayAssociation,
+                               data = data))
+  sfStop()
+  cat("Finished at ", format(Sys.time(),usetz = TRUE), '\n')
+  as.data.frame(result)
+}
+
+#' Computes a single set of 3 way association estimates for a given model.
+#' 
+#' @param model The triplet of column indices to use.
+#' @param data The dataframe.
+#' @return A dataframe.
+threeWayAssociation <- function(model, data){
+  results <- data.frame(t(rep(0, 5)))
+  colnames(results) = c('dcor', 'A', 'R2', 'nldcor', 'nlA')
+  X <- unlist(model[1])
+  Y <- unlist(model[2])
+  Z <- unlist(model[3])
+    
+  #linear and nonlinear association
+  results$dcor <- dcov.test(data[,X],data[,c(Y,Z)])$estimates[2]
+  results$A <- ma(data.frame(data[,c(Y,Z)],data[,X]))$A
+  res <- lm(data[,X] ~ data[,Y] * data[,Z])
+  results$R2 <- summary(res)$r.squared
+    
+  #residual nonlinear association
+  results$nldcor <- dcov.test(res$residuals,data[,c(Y,Z)])$estimates[2]
+  results$nlA <- ma(data.frame(data[,c(Y,Z)],res$residuals))$A
+    
   results
 }
+
+
+threeWayAssociation(pr[1,], data = data.frame(genes[, -c(1,96,97)]))
+threeWayAll(pr[1:10,], data = data.frame(genes[, -c(1,96,97)]))
 
 #evaluate the 3 way associations separately for each group and also combined.
 control3way <- threeWay(dd, controls[,-c(1,96)]) 
